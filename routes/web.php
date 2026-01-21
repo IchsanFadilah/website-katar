6c64072284210e91fbc354a1573f6864e099b481
Ichsan Fadilah <ichsanfadilah1@gmail.com>
2026-01-21 16:14:14 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Livewire\About;
+use App\Livewire\Admin\Admin;
 use App\Livewire\Contact;
 use App\Livewire\Home;
 use App\Livewire\Struktur;
@@ -13,3 +14,5 @@ Route::get('/kegiatan', \App\Livewire\Event::class)->name('event');
 Route::get('/kontak', Contact::class)->name('contact');
 Route::get('/visi-misi', VisiMisi::class)->name('visi-misi');
 Route::get('/struktur-organisasi', Struktur::class)->name('struktur');
+
+Route::get('/admin', Admin::class)->name('admin');
